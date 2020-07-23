625572e1a274d5af4f470062589612149b38a456
Estela Romero Muñoz <estelaromer@gmail.com>
2020-07-23 15:48:09 +0200

    Route to post a task

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -6,6 +6,12 @@ const Task = require('../models/Task');
 router.get('/', async (req, res) => {
     const tasks = await Task.find();
     res.json(tasks);
-})
+});
+
+router.post('/', async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+    res.json({"status": "Received"});
+});
 
 module.exports = router;
