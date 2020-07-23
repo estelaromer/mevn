6467672fa04670e5b66ea54c2c4e4335537b68cb
Estela Romero Muñoz <estelaromer@gmail.com>
2020-07-23 07:25:41 +0200

    Task schema created and first query add to route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
 const app = express();
+mongoose.connect('mongodb://localhost/mevn-database')
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.error(err));
 
 // Settings
 app.set('port', process.env.PORT || 3000);
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('API tasks go here');
+const Task = require('../models/Task');
+
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
 })
 
 module.exports = router;
